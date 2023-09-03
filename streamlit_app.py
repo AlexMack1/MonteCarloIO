a8866e4bd499472f6647abf805fb006c101989f9
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-02 20:25:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define tus funciones de simulación aquí
+
+# Crear la interfaz de la aplicación Streamlit
+st.title('Simulación de Montecarlo para Estrategia de Inversión')
+st.write('Esta aplicación realiza una simulación de Montecarlo para una estrategia de inversión de 17 años.')
+
+# Entradas del usuario
+capital_inicial = st.number_input('Capital Inicial', value=0)
+inversion_anual = st.number_input('Inversión Anual', value=20000)
+anos = st.number_input('Años', value=17)
+rendimiento_promedio = st.number_input('Rendimiento Promedio', value=0.04)
+desviacion_estandar = st.number_input('Desviación Estándar', value=0.10)
+num_simulaciones = st.number_input('Número de Simulaciones', value=1000)
+
+# Realizar la simulación
+if st.button('Realizar Simulación'):
+    resultados = montecarlo_simulacion(anos, capital_inicial, inversion_anual, rendimiento_promedio, desviacion_estandar, num_simulaciones)
+    # Analizar y mostrar los resultados
+    # ...
+
+# Mostrar gráficos
+if st.button('Mostrar Gráficos'):
+    # Crear y mostrar gráficos
+    # ...
 
